5dd73124b9f37a2d15be509e4c078b159cedc933
yaako256 <281857787+yaako256@users.noreply.github.com>
2026-06-02 08:17:57 +0900

    feat: #6 共通のエラー型を定義する

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
+/*
+crates/app/src/main.rs
+コマンド引数を受け取り、kernelに処理を委譲する
+*/
+// エラー型用
+use shared::errors::{AppError, AppResult};
+
+fn main() -> AppResult<()> {
   println!("Hello, world!");
   kernel::debug();
   shared::debug();
@@ -8,4 +15,6 @@ fn main() {
   logger::debug();
   news_fetch::debug();
   llm::debug();
+
+  Ok(())
 }
--- crates/shared/src/lib.rs
+++ crates/shared/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod errors;
+
 pub fn debug() {
   println!("Hello, shared!")
 }
