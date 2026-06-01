f81c93f0b152d51ce342f7865075069e6ae6d5cb
yaako256 <281857787+yaako256@users.noreply.github.com>
2026-06-02 04:44:53 +0900

    feat: #4 workspaceのクレートを全部作成する

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
   println!("Hello, world!");
+  kernel::debug();
+  shared::debug();
+  config::debug();
+  notifier::debug();
+  infra::debug();
+  logger::debug();
+  news_fetch::debug();
+  llm::debug();
 }
---
+++ crates/news_fetch/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn debug() {
+  println!("Hello, news_fetch!")
+}
