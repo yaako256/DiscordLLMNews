482ae50eef19e556e40e846c1a1ea4aa31471f25
yaako256 <281857787+yaako256@users.noreply.github.com>
2026-06-03 07:22:49 +0900

    feat; #16 loggerを作成する

--- crates/logger/src/lib.rs
+++ crates/logger/src/lib.rs
@@ -1,3 +1,8 @@
-pub fn debug() {
-  println!("Hello, logger!")
-}
+/*
+crates/logger/src/lib.rs
+グローバルで持つloggerの定義
+*/
+use std::sync::{Mutex, OnceLock};
+mod model;
+
+pub use model::NotificationLogLogger;
